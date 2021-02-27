356190dc1e5c4e733239d9ac079f0140f01762d6
Wanderson Camargo <wandersonwhcr@gmail.com>
2021-02-27 10:32:24 -0300

    Efetua conexão com AMQP

--- projects/handler/server.js
+++ projects/handler/server.js
@@ -1,14 +1,31 @@
 const http = require("http");
 const uuid = require("uuid");
+const amqp = require("amqplib");
 
-const server = http.createServer((req, res) => {
-  const requestId = uuid.v4();
+const handler = async () => {
+  // AMQP Connection
+  const connection = await amqp.connect("amqp://broker");
+  // AMQP Channel
+  const channel = await connection.createChannel();
+  // Assert Request and Response Queue
+  channel.assertQueue("request");
+  channel.assertQueue("response");
 
-  res.writeHead(200, {
-    'X-Request-Id': requestId,
+  // HTTP Server
+  const server = http.createServer((req, res) => {
+    // X-Request-Id
+    const requestId = uuid.v4();
+    // Response Message Header
+    res.writeHead(200, {
+      "X-Request-Id": requestId,
+    });
+    // Done!
+    res.end();
   });
 
-  res.end();
-});
+  // Server Listen
+  server.listen(process.env.PORT, '0.0.0.0');
+};
 
-server.listen(process.env.PORT, '0.0.0.0');
+handler()
+  .catch(console.warn);
