1a4897b5034d21af18130d049e74342f6d373ecd
Wanderson Camargo <wandersonwhcr@gmail.com>
2021-02-27 10:04:35 -0300

    Inicializa servidor HTTP utilizando Node.js

--- projects/handler/server.js
+++ projects/handler/server.js
@@ -1,1 +1,8 @@
-console.log("Hello, World!");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('OK');
+});
+
+server.listen(process.env.PORT, '0.0.0.0');
